97900292a403d746943294083f97c3abb5f98e04
Timothy R. McReynolds <31673072+TimMcR@users.noreply.github.com>
2026-03-09 14:49:18 -0500

    wip: trough menu

--- src/main/java/net/Rampage/mob_block_farming/block/entity/custom/TroughBlockEntity.java
+++ src/main/java/net/Rampage/mob_block_farming/block/entity/custom/TroughBlockEntity.java
@@ -2,15 +2,31 @@ package net.Rampage.mob_block_farming.block.entity.custom;
 
 import net.Rampage.mob_block_farming.block.entity.ModBlockEntities;
 import net.Rampage.mob_block_farming.item.custom.SlurryItem;
+import net.Rampage.mob_block_farming.screen.custom.TroughMenu;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.ClientGamePacketListener;
+import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
+import net.minecraft.world.Containers;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.SimpleContainer;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.ItemStackHandler;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class TroughBlockEntity extends BlockEntity {
-    public final ItemStackHandler itemStackHandler = new ItemStackHandler(1) {
+public class TroughBlockEntity extends BlockEntity implements MenuProvider {
+    public final ItemStackHandler itemHandler = new ItemStackHandler(1) {
         @Override
         public boolean isItemValid(int slot, @NotNull ItemStack stack) {
             return stack.getItem() instanceof SlurryItem;
@@ -25,7 +41,72 @@ public class TroughBlockEntity extends BlockEntity {
         }
     };
 
+    private static final int INPUT_SLOT = 0;
+
+    private LazyOptional<IItemHandler> lazyItemHandler = LazyOptional.empty();
+
+
     public TroughBlockEntity(BlockPos pPos, BlockState pBlockState) {
         super(ModBlockEntities.TROUGH_BE.get(), pPos, pBlockState);
+    }
+
+    @Override
+    public void onLoad() {
+        super.onLoad();
+        lazyItemHandler = LazyOptional.of(() -> itemHandler);
+    }
+
+    @Override
+    public void invalidateCaps() {
+        super.invalidateCaps();
+        lazyItemHandler.invalidate();
+    }
+
+    public void drops() {
+        SimpleContainer inventory = new SimpleContainer(itemHandler.getSlots());
+        for (int i = 0; i < itemHandler.getSlots(); i++) {
+            inventory.setItem(i, itemHandler.getStackInSlot(i));
+        }
+
+        Containers.dropContents(this.level, this.worldPosition, inventory);
+    }
+
+    @Override
+    protected void saveAdditional(CompoundTag pTag, HolderLookup.Provider pRegistries) {
+        pTag.put("inventory", itemHandler.serializeNBT(pRegistries));
+
+        super.saveAdditional(pTag, pRegistries);
+    }
+
+    @Override
+    protected void loadAdditional(CompoundTag pTag, HolderLookup.Provider pRegistries) {
+        super.loadAdditional(pTag, pRegistries);
+
+        itemHandler.deserializeNBT(pRegistries, pTag.getCompound("inventory"));
+    }
+
+    @Override
+    public Component getDisplayName() {
+        return Component.translatable("block.mob_block_farming.trough");
+    }
+
+    @Override
+    public @Nullable AbstractContainerMenu createMenu(int pContainerId, Inventory pPlayerInventory, Player pPlayer) {
+        return new TroughMenu(pContainerId, pPlayerInventory, this);
+    }
+
+    public ItemStack getItems() {
+        return itemHandler.getStackInSlot(INPUT_SLOT);
+    }
+
+    @Override
+    public CompoundTag getUpdateTag(HolderLookup.Provider pRegistries) {
+        return saveWithoutMetadata(pRegistries);
+    }
+
+    @Nullable
+    @Override
+    public Packet<ClientGamePacketListener> getUpdatePacket() {
+        return ClientboundBlockEntityDataPacket.create(this);
     }
 }
---
+++ src/main/java/net/Rampage/mob_block_farming/screen/custom/TroughScreen.java
@@ -0,0 +1,17 @@
+package net.Rampage.mob_block_farming.screen.custom;
+
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Inventory;
+
+public class TroughScreen extends AbstractContainerScreen<TroughMenu> {
+    public TroughScreen(TroughMenu pMenu, Inventory pPlayerInventory, Component pTitle) {
+        super(pMenu, pPlayerInventory, pTitle);
+    }
+
+    @Override
+    protected void renderBg(GuiGraphics pGuiGraphics, float pPartialTick, int pMouseX, int pMouseY) {
+
+    }
+}
