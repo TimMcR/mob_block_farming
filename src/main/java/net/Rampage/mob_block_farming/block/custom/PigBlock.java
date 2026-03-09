216680c2de5f263e2b4c97f13ecb5ca3c28e4b8a
Timothy R. McReynolds <31673072+TimMcR@users.noreply.github.com>
2026-03-09 17:00:19 -0500

    feat: pig block eating vegan slurry from north trough

--- src/main/java/net/Rampage/mob_block_farming/block/custom/PigBlock.java
+++ src/main/java/net/Rampage/mob_block_farming/block/custom/PigBlock.java
@@ -1,16 +1,20 @@
 package net.Rampage.mob_block_farming.block.custom;
 
 import com.mojang.serialization.MapCodec;
+import net.Rampage.mob_block_farming.block.entity.ModBlockEntities;
 import net.Rampage.mob_block_farming.block.entity.custom.PigBlockEntity;
 import net.Rampage.mob_block_farming.util.MobType;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
@@ -55,5 +59,16 @@ public class PigBlock extends BaseEntityBlock {
     @Override
     protected RenderShape getRenderShape(BlockState pState) {
         return RenderShape.MODEL;
+    }
+
+    @Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level pLevel, BlockState pState, BlockEntityType<T> pBlockEntityType) {
+        if(pLevel.isClientSide())
+            return null;
+
+        return createTickerHelper(pBlockEntityType, ModBlockEntities.PIG_BLOCK_BE.get(),
+                (level, blockPos, blockState,
+                 pigBlockEntity) -> pigBlockEntity.tick(level, blockPos, blockState));
     }
 }
--- src/main/java/net/Rampage/mob_block_farming/block/entity/custom/PigBlockEntity.java
+++ src/main/java/net/Rampage/mob_block_farming/block/entity/custom/PigBlockEntity.java
@@ -1,15 +1,109 @@
 package net.Rampage.mob_block_farming.block.entity.custom;
 
 import net.Rampage.mob_block_farming.block.entity.ModBlockEntities;
+import net.Rampage.mob_block_farming.item.ModItems;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.ClientGamePacketListener;
+import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.inventory.ContainerData;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.items.IItemHandler;
+import org.jetbrains.annotations.Nullable;
 
 public class PigBlockEntity extends BlockEntity {
+    protected final ContainerData data;
+
     private int foodPoints = 0;
     private int eatTimer = 0;
 
+    private static final int TICK_INTERVAL = 20;
+
     public PigBlockEntity(BlockPos pPos, BlockState pBlockState) {
         super(ModBlockEntities.PIG_BLOCK_BE.get(), pPos, pBlockState);
+
+        data = new ContainerData() {
+            @Override
+            public int get(int pIndex) {
+                return switch (pIndex) {
+                    case 0 -> PigBlockEntity.this.foodPoints;
+                    case 1 -> PigBlockEntity.this.eatTimer;
+                    default -> 0;
+                };
+            }
+
+            @Override
+            public void set(int i, int value) {
+                switch (i) {
+                    case 0: PigBlockEntity.this.foodPoints = value;
+                    case 1: PigBlockEntity.this.eatTimer = value;
+                }
+            }
+
+            @Override
+            public int getCount() {
+                return 2;
+            }
+        };
+    }
+
+    public void tick(Level level, BlockPos blockPos, BlockState blockState) {
+        Direction frontDirection = blockState.getValue(BlockStateProperties.HORIZONTAL_FACING);
+
+        BlockPos frontPos = this.worldPosition.relative(frontDirection);
+
+        BlockEntity northBlock = level.getBlockEntity(frontPos);
+
+        if (northBlock instanceof TroughBlockEntity trough) {
+            LazyOptional<IItemHandler> capability = trough.getCapability(ForgeCapabilities.ITEM_HANDLER);
+
+            capability.ifPresent(handler -> {
+                ItemStack stack = handler.getStackInSlot(0);
+
+                if (stack.is(ModItems.VEGAN_SLURRY.get())) {
+                    eatTimer++;
+                    setChanged(level, blockPos, blockState);
+
+                    if (eatTimer == TICK_INTERVAL) {
+                        handler.extractItem(0, 1, false);
+                        foodPoints += 5;
+                        level.playSound(null, blockPos, SoundEvents.PLAYER_BURP, SoundSource.BLOCKS);
+                        resetEatingProgress();
+                    }
+                }
+                else {
+                    resetEatingProgress();
+                }
+            });
+        }
+        else {
+            resetEatingProgress();
+        }
+    }
+
+    private void resetEatingProgress() {
+        eatTimer = 0;
+    }
+
+    @Override
+    public CompoundTag getUpdateTag(HolderLookup.Provider pRegistries) {
+        return saveWithoutMetadata(pRegistries);
+    }
+
+    @Nullable
+    @Override
+    public Packet<ClientGamePacketListener> getUpdatePacket() {
+        return ClientboundBlockEntityDataPacket.create(this);
     }
 }
