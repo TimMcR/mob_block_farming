fb79817f02c710cb2d983aa4e7183e8f0629f54d
Timothy R. McReynolds <31673072+TimMcR@users.noreply.github.com>
2026-03-09 14:11:39 -0500

    feat: make slurry items have access to a slurry type

--- src/main/java/net/Rampage/mob_block_farming/item/ModItems.java
+++ src/main/java/net/Rampage/mob_block_farming/item/ModItems.java
@@ -1,6 +1,8 @@
 package net.Rampage.mob_block_farming.item;
 
 import net.Rampage.mob_block_farming.MobBlockFarming;
+import net.Rampage.mob_block_farming.item.custom.SlurryItem;
+import net.Rampage.mob_block_farming.util.SlurryType;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -14,9 +16,9 @@ public class ModItems {
     public static final RegistryObject<Item> ROTARY_BLADE =
             ITEMS.register("rotary_blade", () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> VEGAN_SLURRY =
-            ITEMS.register("vegan_slurry", () -> new Item(new Item.Properties()));
+            ITEMS.register("vegan_slurry", () -> new SlurryItem(new Item.Properties(), SlurryType.VEGAN));
     public static final RegistryObject<Item> MEAT_SLURRY =
-            ITEMS.register("meat_slurry", () -> new Item(new Item.Properties()));
+            ITEMS.register("meat_slurry", () -> new SlurryItem(new Item.Properties(), SlurryType.MEAT));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
